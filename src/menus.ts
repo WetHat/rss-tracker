90bb2ae6c55afd409b284562106f8c3cf0d5abeb
WetHat <PeterErnst@outlook.de>
2024-05-21 10:17:44 +0200

    TsDoc added

--- src/menus.ts
+++ src/menus.ts
@@ -1,6 +1,17 @@
 import RSSTrackerPlugin from './main';
-import { App, Notice, Menu, TFile } from 'obsidian';
+import { App, Notice, Menu, TFile, EventRef } from 'obsidian';
 
+/**
+ * Utility class to conditionally add an item to Obsidian menus.
+ *
+ * The condition is that the current file is a RSS dashboard which
+ * contains frontmatter defiing the feed configuration.
+ *
+ * Currently event menu event hadlers can be generated for the file menu
+ * @see {link fileMenuHandler} and the ditor menu
+ * @see {link editorMenuHandler}.
+ *
+ */
 export class UpdateRSSfeedMenuItem {
     protected app: App;
     protected plugin: RSSTrackerPlugin;
@@ -10,7 +21,17 @@ export class UpdateRSSfeedMenuItem {
         this.plugin = plugin;
     }
 
-    private addItem(menu : Menu, file : TFile | null) {
+    /**
+     * Condidionally add an item to a menu which calls an action to update an RSS feed.
+     *
+     * The condition under which the item is added is that the given file is an
+     * RSS dashboard containing frontmatter defining the properties `itemlimit` and
+     * `feedurl`.
+     *
+     * @param menu - The menu to add the item to
+     * @param file - An Obsidian file which may contain frontmatter describing an RSS feed configuration.
+     */
+    private addItem (menu: Menu, file: TFile | null) {
         if (file) {
             const feedconfig = this.plugin.feedmgr.getFeedConfig(file);
             if (feedconfig) {
@@ -18,19 +39,27 @@ export class UpdateRSSfeedMenuItem {
                     item.setTitle('Update RSS feed')
                         .setIcon('rss')
                         .onClick(async () => {
-                            new Notice(`${file?.name ?? 'unavailable'  } updated`);
+                            new Notice(`${file?.name ?? 'unavailable'} updated`);
                         });
                 });
             }
         }
     }
 
-    get editorMenuHandler() {
+    /**
+     * Event handler for the the _editor_ context menu to
+     * conditionally add a menu item.
+     */
+    get editorMenuHandler (): EventRef {
         return this.app.workspace.on('editor-menu', (menu, editor, view) => {
-            this.addItem(menu,view?.file);
+            this.addItem(menu, view?.file);
         });
     }
-    get fileMenuHandler() {
+    /**
+     * Event handler for the the _file_ context menu to
+     * conditionally add a menu item.
+     */
+    get fileMenuHandler (): EventRef {
         return this.app.workspace.on('file-menu', (menu, file) => {
             this.addItem(menu, file instanceof TFile ? file : null);
         });
