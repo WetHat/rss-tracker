10aa3080c122ac4195736e0bdc45a0a8bc39c25e
WetHat <PeterErnst@outlook.de>
2024-05-26 15:07:49 +0200

    Scripts updated for new script and reference data location

--- pack.mjs
+++ pack.mjs
@@ -1,7 +1,5 @@
-import {copyFileSync,accessSync,constants,renameSync} from 'fs'
-import {join } from 'path';
-import {execSync} from "child_process";
-import { stderr } from 'process';
+import { copyFileSync, accessSync, constants, renameSync } from 'fs'
+import { join } from 'path';
 
 // copy assets
 console.log("Copying assets...");
@@ -9,22 +7,22 @@ console.log("Copying assets...");
     "manifest.json",
     "styles.css"
 ].forEach(f => {
-    const target = join("./dist",f);
+    const target = join("./dist", f);
     console.log(`${f} => ${target}`);
-    copyFileSync(f,target);
+    copyFileSync(f, target);
 });
 
 // configure test code
 console.log("Configuring regression tests...");
 try {
-    accessSync("./test/FeedAssembler.js", constants.F_OK);
+    accessSync("./test-vault/scripts/FeedAssembler.js", constants.F_OK);
 } catch (err) {
     process.exit(0);
 }
 
 try {
-    renameSync("./test/FeedAssembler.js", "./test/FeedAssembler.mjs");
-    console.log("src/FeedAssembler.ts => test/FeedAssembler.mjs");
+    renameSync("./test-vault/scripts/FeedAssembler.js", "./test-vault/scripts/FeedAssembler.mjs");
+    console.log("src/FeedAssembler.ts => test-vault/scripts/FeedAssembler.mjs");
 } catch (err) {
     console.error(err);
     process.exit(1);
