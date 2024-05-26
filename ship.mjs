065d6105e3b86f9b2d400cad3e3d495d7a1898fa
WetHat <PeterErnst@outlook.de>
2024-05-26 12:49:40 +0200

    script adjusted to ship to test-vault

--- ship.mjs
+++ ship.mjs
@@ -3,20 +3,10 @@
  */
 import * as path from "path";
 import * as fs from "fs";
-import * as dotenv from "dotenv";
 
 let manifest = JSON.parse(fs.readFileSync("manifest.json", "utf8"));
 
-// Determine the deployment location in the obsidian vault
-// configured in .env
-const env = dotenv.config();
-const VAULT = env.parsed.VAULT;
-if (!VAULT || VAULT.trim().length === 0) {
-	console.error("VAULT undefined! Set VAULT in `.env`.");
-	process.exit(1);
-}
-
-const targetDir = path.join(VAULT, ".obsidian", "plugins", manifest["id"]);
+const targetDir = path.join("test-vault", ".obsidian", "plugins", manifest["id"]);
 if (!fs.existsSync(targetDir)) {
     console.warn(`Creating targetDir directory:  ${targetDir}`);
 	fs.mkdirSync(targetDir);
