78ee1892522840cb90bf5eacd94e4e3fd654e24e
WetHat <PeterErnst@outlook.de>
2024-05-27 18:58:20 +0200

    Mocha test environment added & parser test script moved to test/scripts

--- pack.mjs
+++ pack.mjs
@@ -15,14 +15,14 @@ console.log("Copying assets...");
 // configure test code
 console.log("Configuring regression tests...");
 try {
-    accessSync("./test-vault/scripts/FeedAssembler.js", constants.F_OK);
+    accessSync("./test/scripts/FeedAssembler.js", constants.F_OK);
 } catch (err) {
     process.exit(0);
 }
 
 try {
-    renameSync("./test-vault/scripts/FeedAssembler.js", "./test-vault/scripts/FeedAssembler.mjs");
-    console.log("src/FeedAssembler.ts => test-vault/scripts/FeedAssembler.mjs");
+    renameSync("./test/scripts/FeedAssembler.js", "./test/scripts/FeedAssembler.mjs");
+    console.log("src/FeedAssembler.ts => test/scripts/FeedAssembler.mjs");
 } catch (err) {
     console.error(err);
     process.exit(1);
