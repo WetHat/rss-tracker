d46b2059d2f3530ac8af27d007752da8411961d9
WetHat <PeterErnst@outlook.de>
2025-05-27 12:12:54 +0200

    Test data updates after changes for issues: #7 & #9

--- test-vault/.obsidian/plugins/rss-tracker/settingsUI.d.ts
+++
@@ -1,8 +0,0 @@
-import { PluginSettingTab } from "obsidian";
-import { RSSTrackerSettings } from "./settings";
-export declare class RSSTrackerSettingTab extends PluginSettingTab {
-    settings: RSSTrackerSettings;
-    constructor(settings: RSSTrackerSettings);
-    display(): void;
-    hide(): void;
-}
